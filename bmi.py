a6f83c1f5b1b1e0634c3841762f6fcb132672bdb
howtow <playgames1911@gmail.com>
2021-10-13 13:59:09 +0800

    complete file

--- bmi.py
+++ bmi.py
@@ -1,3 +1,5 @@
-height = input('請問身高:')
-weight = input('請問體重:')
+height = float(input('請問身高:')) #123123
+weight = float(input('請問體重:'))
 print('身高為:', height,'體重為:', weight)
+bmi = float(weight / (height/100)**2)
+print('你的bmi是:', bmi)
